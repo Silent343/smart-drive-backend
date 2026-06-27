11a810d4b7242b3db50725aac39c7c4e359151d3
Silent343 <gabrielfgs2004@gmail.com>
2026-06-27 01:53:31 -0500

    fea: added health controller

---
+++ src/main/java/pe/edu/upc/smartdrive/platform/shared/interfaces/rest/HealthController.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.smartdrive.platform.shared.interfaces.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, String>> health() {
+        return ResponseEntity.ok(Map.of("status", "UP"));
+    }
+}
